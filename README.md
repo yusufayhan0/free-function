8b51551ceb1abe0ae6de062286fd65a3cca81f42
Yusuf AYHAN <yusufay.ay1@gmail.com>
2024-03-23 18:22:36 +0300

    first commit :)

---
+++ README.md
@@ -0,0 +1,40 @@
+# Free Function
+
+This extension provides developers in React projects with a simple way to create functions, enhancing efficiency and effectiveness in coding.
+
+## Features
+
+The main features of it extension:
+
+- Automatic Function Generation: Automatically generates a function based on the parameters provided. Simply specify the parameters you want, and the function is created automatically.
+
+- Easy Usage: It's incredibly easy to use in React projects. Just write the code to call a function.
+
+- Rapid Definition: There's no need to write complex code to create a function. You can quickly define a function based on the given parameters.
+
+ ## How to use
+
+ ![How to use](./src/assets/images/how-to-use.gif)
+
+## Installation
+
+1. Open Visual Studio Code.
+2. Click on the Extensions icon on the left menu (or use the `Ctrl+Shift+X` shortcut).
+3. Type `"Free Function"` in the search bar and find your extension.
+4. Click the Install button to install the extension.
+
+## Usage
+
+Provide instructions on how to use the extension here. This could include commands, shortcuts, or menu options that provide access to features.
+
+## Settings
+
+There is currently no settings section available, but it will be included in future updates.
+
+## Contributing
+
+--
+
+## License
+
+`This extension is licensed under the [MIT License](LICENSE).`
---
+++ src/extension.js
@@ -0,0 +1,102 @@
+const vscode = require('vscode');
+
+function activate(context) {
+    const provider = vscode.languages.registerCompletionItemProvider('javascript', {
+        provideCompletionItems(document, position) {
+            // İmlecin bulunduğu satırı al
+            const linePrefix = document.lineAt(position).text.substr(0, position.character);
+            
+            // Regex ile fonksiyon çağrısı pattern'ini kontrol et
+            const functionCallPattern = /\b(\w+)\(([^)]*)\)\s$/;
+            const match = linePrefix.match(functionCallPattern);
+            
+            if (!match) {
+                return undefined;
+            }
+            
+            const completionItem = new vscode.CompletionItem('Create Function', vscode.CompletionItemKind.Snippet);
+            completionItem.insertText = '';
+            match.shift()
+
+            // Kullanıcı bu öneriyi seçtiğinde, komutu tetikle
+            completionItem.command = { command: 'extension.createFunction', title: 'Create Function', arguments: [...match] };
+            return [completionItem];
+        },
+    }, ' '); // Boşluk karakteri üzerinde tetiklenir
+
+    context.subscriptions.push(provider);
+
+    // Komutu kaydet
+    const command = vscode.commands.registerCommand('extension.createFunction', (funcName, paramsText) => {
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return;
+        }
+        
+        const params = paramsText.split(',')
+        .map((param, index) => {
+          return (
+            param.includes('\'') || 
+            param.includes('"') ||
+            param.includes('`')
+          ) 
+          ? `arg${(index + 1)}` 
+          : param.trim()
+        })
+
+        const document = editor.document;
+        const position = editor.selection.active; // İmlecin aktif pozisyonu
+        let line = position.line;
+
+        // İmleçten yukarı doğru giderek en yakın component tanımını bul
+        let componentStart = -1;
+        for (; line >= 0; line--) {
+            const lineText = document.lineAt(line).text;
+            if (lineText.match(/function\s+\w+\s*\([^)]*\)\s*{/)) {
+                componentStart = line;
+                break;
+            }
+        }
+
+        if (componentStart === -1) {
+            vscode.window.showErrorMessage('No component found');
+            return;
+        }
+
+        // Component tanımından itibaren 'return' ifadesini bul
+        const returnRegex = /return\s*\(/;
+        let returnPosition = null;
+        let functionPosition = null;
+        for (; line < document.lineCount; line++) {
+            const lineText = document.lineAt(line).text;
+            
+            if (lineText.match(returnRegex)) {
+                returnPosition = new vscode.Position(line - 1, 0);
+                functionPosition = new vscode.Position(line - 2, 0);
+                break;
+            }
+        }
+
+        if (!returnPosition) {
+            vscode.window.showErrorMessage('No return statement found in the component');
+            return;
+        }
+
+        const snippet = `\tfunction ${funcName}(${params.join(',')}) {\n\t\t// Your code here\n\t}\n`;
+
+        editor.edit(editBuilder => {
+            editBuilder.insert(returnPosition, snippet);
+            editor.selection = new vscode.Selection(functionPosition, functionPosition);
+            editor.revealRange(new vscode.Range(functionPosition, functionPosition));
+        });
+    });
+
+    context.subscriptions.push(command);
+}
+
+function deactivate() {}
+
+module.exports = {
+    activate,
+    deactivate
+};
